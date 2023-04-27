049e28b39610b520658447aeaecefa844f3e7b16
Maks Kravchenko <krava.zf@gmail.com>
2023-04-27 15:17:58 +0300

    output-using-a-template-string

--- markap/index.js
+++ markap/index.js
@@ -2,4 +2,4 @@ firstNumber = parseFloat(prompt('Enter the first number'));
 secondNumber = parseFloat(prompt('Enter the second number'));
 thirdNumber = parseFloat(prompt('Enter the third number'));
 arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
-alert('Arithmetic Mean: ' + arithmeticMean);
+alert(`Arithmetic Mean: ${arithmeticMean}`);пше 
