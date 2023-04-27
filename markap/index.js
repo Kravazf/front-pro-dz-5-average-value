f33f2655e8fc4040b088addd993c6151c6e517d2
Maks Kravchenko <krava.zf@gmail.com>
2023-04-27 14:47:02 +0300

    dz5-average-value-Done

---
+++ markap/index.js
@@ -0,0 +1,5 @@
+firstNumber = parseFloat(prompt('Enter the first number'));
+secondNumber = parseFloat(prompt('Enter the second number'));
+thirdNumber = parseFloat(prompt('Enter the third number'));
+arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+alert('Arithmetic Mean: ' + arithmeticMean);
